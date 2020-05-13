14e528c39a8bfd2ce5b5dc633371d6d74d37c3be
Keviiiiin <just4github@outlook.com>
2020-05-13 23:20:01 +0800

    测试

--- src/test/java/com/icckevin/community/MapperTests.java
+++ src/test/java/com/icckevin/community/MapperTests.java
@@ -2,6 +2,7 @@ package com.icckevin.community;
 
 import com.icckevin.community.dao.DiscussPostMapper;
 import com.icckevin.community.dao.UserMapper;
+import com.icckevin.community.entity.DiscussPost;
 import com.icckevin.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * @description:
@@ -32,5 +35,16 @@ public class MapperTests {
 
         User user = userMapper.selectById(101);
         System.out.println(user);
+    }
+
+    @Test
+    public void testSelectPosts() {
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(149, 0, 10);
+        for(DiscussPost post : list) {
+            System.out.println(post);
+        }
+
+        int rows = discussPostMapper.selectDiscussPostRows(149);
+        System.out.println(rows);
     }
 }
