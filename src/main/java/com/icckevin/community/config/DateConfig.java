65f9e1ae612f7f143faabd103120349754bfdc7c
Keviiiiin <just4github@outlook.com>
2020-05-13 23:38:11 +0800

    增加了日期格式转换

--- src/main/java/com/icckevin/community/config/DateConfig.java
+++ src/main/java/com/icckevin/community/config/DateConfig.java
@@ -1,9 +1,19 @@
 package com.icckevin.community.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+
 /**
  * @description: 日期格式转换
  * @author: iccKevin
  * @create: 2020-05-13 21:17
  **/
+@Component
 public class DateConfig {
+    @Bean
+    public SimpleDateFormat simpleDateFormat() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    }
 }
