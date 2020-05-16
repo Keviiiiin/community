82d29fb5ba8304e3d7757b42eed8f898a235f84d
Keviiiiin <just4github@outlook.com>
2020-05-16 19:02:59 +0800

    添加注册功能

--- src/main/java/com/icckevin/community/controller/LoginController.java
+++ src/main/java/com/icckevin/community/controller/LoginController.java
@@ -1,8 +1,15 @@
 package com.icckevin.community.controller;
 
+import com.icckevin.community.entity.User;
+import com.icckevin.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 /**
  * @description: 注册
@@ -11,8 +18,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
  **/
 @Controller
 public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(value = "/register",method = RequestMethod.GET)
     public String getRegisterPage(){
         return "/site/register";
+    }
+
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    public String register(Model model, User user){
+        Map<String, Object> map = userService.register(user);
+        if(map.isEmpty()){
+            model.addAttribute("msg", "注册成功,我们已经向您的邮箱发送了一封激活邮件,请尽快激活!");
+            model.addAttribute("target", "/index");
+            return "/site/operate-result";
+        }
+        else {
+            model.addAttribute("usernameMsg",map.get("usernameMsg"));
+            model.addAttribute("passwordMsg",map.get("passwordMsg"));
+            model.addAttribute("emailMsg",map.get("emailMsg"));
+            return "/site/register";
+        }
+
     }
 }
--- src/main/java/com/icckevin/community/service/UserService.java
+++ src/main/java/com/icckevin/community/service/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import java.util.Date;
 import java.util.HashMap;
