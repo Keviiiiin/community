f0fddfbccc923bf03960e8c76e40ca9a65d8f8a6
Keviiiiin <just4github@outlook.com>
2020-05-26 11:52:56 +0800

    过滤敏感词

---
+++ src/test/java/com/icckevin/community/SensitiveTest.java
@@ -0,0 +1,34 @@
+package com.icckevin.community;
+
+import com.icckevin.community.utils.SensitiveFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @description:
+ * @author: iccKevin
+ * @create: 2020-05-26 11:44
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTest {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testFilter() {
+        String text = "这里可以赌博,可以嫖娼,可以吸毒,可以开票,哈哈哈!";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "这里可以开*票,可以☆嫖☆娼☆,可以☆吸☆毒☆,可以☆开☆票☆,哈哈哈!";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+}
